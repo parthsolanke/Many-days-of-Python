8b74f217c92d7c138a7bea56b9336b370bbd2fac
Parth Solanke <parthsolanke2002@gmail.com>
2023-10-12 17:16:42 +0530

    NATO Alphabet

---
+++ NATO-alphabet/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+df = pd.read_csv(r"NATO-alphabet\data\nato_phonetic_alphabet.csv")
+
+nato_dict = {row.letter:row.code for (index, row) in df.iterrows()}
+
+# creating nato dictionary
+inp = input("Enter a word: ").upper()
+lst  = [nato_dict[letter] for letter in inp]
+print(lst)
+
+# NOTE: Loop through rows of a data frame
+# for (index, row) in df.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+
+# NOTE: Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
