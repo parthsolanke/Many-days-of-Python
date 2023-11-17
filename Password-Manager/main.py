21f1d63843fcd3165b491450b938a3dc0d0d6697
Parth Solanke <parthsolanke2002@gmail.com>
2023-11-17 20:30:46 +0530

    modified project with json file format and error handeling practices

--- Password-Manager/main.py
+++ Password-Manager/main.py
@@ -46,18 +46,43 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Warning!", message="Make sure to fill each field.")
     else:
-        with open(r"Password-Manager\data\data.json", "r") as file:
-            # reading old data
-            data = json.load(file)
+        try:
+            with open(r"Password-Manager\data\data.json", "r") as file:
+                # reading old data
+                data = json.load(file)
+        except FileNotFoundError:
+            with open(r"Password-Manager\data\data.json", "w") as file:
+                # saving updated data in json file
+                json.dump(json_dict, file, indent=4)
+        else:
             # updating old data with new data
             data.update(json_dict)
             
-        with open(r"Password-Manager\data\data.json", "w") as file:
-            # saving updated data in json file
-            json.dump(data, file, indent=4)
-            
+            with open(r"Password-Manager\data\data.json", "w") as file:
+                # saving updated data in json file
+                json.dump(data, file, indent=4)
+        finally:
             website_entry.delete(0, tk.END)
             password_entry.delete(0, tk.END)
+
+def find_password():
+        
+    website = website_entry.get()
+    
+    try:
+        with open(r"Password-Manager\data\data.json", "r") as file:
+            # reading old data
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Website: {website}"
+                                f"\nEmail: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 window = tk.Tk()
 window.title("Password Manager")
@@ -80,7 +105,7 @@ website_entry.focus()
 
 email_entry = tk.Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, padx=2, pady=2)
-email_entry.insert(0, "parthsolanke@gmail.com")
+email_entry.insert(0, "DummyEmail@gmail.com")
 
 password_entry = tk.Entry(width=35)
 password_entry.grid(row=3, column=1, columnspan=2, padx=2, pady=2)
@@ -93,5 +118,9 @@ generate_button.config(bd=0, bg="#ADD8E6")
 add_button = tk.Button(text="Add", width=10, command=save)
 add_button.grid(row=4, column=1, columnspan=2, padx=2, pady=2)
 add_button.config(bd=0, bg="#ADD8E6")
+
+search_button = tk.Button(text="Search", width=30, command=find_password)
+search_button.grid(row=0, column=1, columnspan=3, padx=2, pady=2)
+search_button.config(bd=0, bg="#ADD8E6")
 
 window.mainloop()
