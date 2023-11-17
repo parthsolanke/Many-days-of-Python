4548f99bec1125aafcc9c0aa14ac921c6d7b613a
Parth Solanke <parthsolanke2002@gmail.com>
2023-11-17 19:41:07 +0530

    modified with try, except and else blocks

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,20 +1,20 @@
 import pandas as pd
 df = pd.read_csv(r"NATO-alphabet\data\nato_phonetic_alphabet.csv")
 
+# creating nato dictionary
 nato_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
-# creating nato dictionary
-
-inp = input("Enter a word: ").upper()
-
-try:
-    lst  = [nato_dict[letter] for letter in inp]
-    
-except KeyError:
-    print("Sorry, only letters in the alphabet please.")
-    
-else: 
-    print(lst)
+def generate_phonetic():
+    inp = input("Enter a word: ").upper()
+    try:
+        lst  = [nato_dict[letter] for letter in inp]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else: 
+        print(lst)
+        
+generate_phonetic()
 
 # NOTE: Loop through rows of a data frame
 # for (index, row) in df.iterrows():
